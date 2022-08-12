c740390688b66cef64aecf4c5650af5c4947c04d
nwkyz <77189398+nwkyz@users.noreply.github.com>
2022-08-12 18:49:55 +0800

    Update 155 PreBeta

---
+++ beta/assets/scripts/config.js
@@ -0,0 +1,14 @@
+DataConnectors = [
+];
+
+var PageFabricConfig = {
+disqusId : "",
+googleAnalyticsId : "",
+globalSearchMode : "Json",
+globalSearchWebServiceURL : "",
+globalSearchLanguageFiles:[
+{
+ lang: "zh", 
+ url: "/zh.json"
+}]
+};
